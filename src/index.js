ae987bf7b999762cd77521c7f7ccb7ce3a40a4b6
harrietgbw <hboardmanwhite@gmail.com>
2024-09-29 16:43:11 +0100

    Added basic form handling and typewriter plug-in

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function generatedPoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "This is the poem",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatedPoem);
